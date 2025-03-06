9e38a598d1698771855cc1fcfc6c5fe882201778
camila.moreira <camilammoreira1@gmail.com>
2025-03-06 11:19:12 -0300

    add item to list

---
+++ script.js
@@ -0,0 +1,29 @@
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+
+const generateTemplate = (todo) => {
+
+    // Create complex element (LI) with html
+    const html = `
+     <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+
+    // Append created element to UL
+    list.innerHTML += html;
+
+}
+
+addForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const todo = addForm.add.value.trim();
+
+    // Check if input is not empty (empty returns 0)
+    if (todo.length) {
+        generateTemplate(todo)
+        addForm.reset(); // Reset all input filed in forms
+    }
+});
