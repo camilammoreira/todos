8e740ffef8d721442625e9365fc4d151d2f3cb3b
camila.moreira <camilammoreira1@gmail.com>
2025-03-07 12:20:16 -0300

    add filter and search todos

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
+let items = [];
 
 const generateTemplate = (todo) => {
 
@@ -13,9 +15,9 @@ const generateTemplate = (todo) => {
 
     // Append created element to UL
     list.innerHTML += html;
-
 }
 
+// Add item
 addForm.addEventListener("submit", e => {
     e.preventDefault();
 
@@ -24,14 +26,40 @@ addForm.addEventListener("submit", e => {
     // Check if input is not empty (empty returns 0)
     if (todo.length) {
         generateTemplate(todo)
+
+        items.push(todo);
+
         addForm.reset(); // Reset all input filed in forms
     }
 });
 
+
+// Delete item
 list.addEventListener("click", e => {
     // Checks if the element I has a delete class
     // Useful to add features, like edit, in future
     if (e.target.classList.contains("delete")) {
         e.target.parentElement.remove();
+
+        const pos = items.indexOf(e.target.previousElementSibling.innerHTML);
+        items.splice(pos, 1);
     }
+});
+
+
+// Filter search
+const filterTodos = (term) => {
+    Array.from(list.children)
+        .filter(todo => !todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => todo.classList.add("hide"));
+
+    Array.from(list.children)
+        .filter(todo => todo.textContent.includes(term))
+        .forEach(todo => todo.classList.remove("hide"));
+}
+
+// Search item
+search.addEventListener("keyup", () => {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
 });
